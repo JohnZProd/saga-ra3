46da9ff7e90ff1c3a8775d5d853a956769c22edc
JohnZProd <zhangjoh@amazon.com>
2021-02-15 01:00:04 +1100

    Added plainclothes cluster

--- infrastructure/cluster.tf
+++ infrastructure/cluster.tf
@@ -12,6 +12,8 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
+data "aws_caller_identity" "current" {}
+
 variable "cluster_role_arn" {
     type = string
 }
@@ -58,4 +60,57 @@ resource "aws_eks_cluster" "cluster" {
   version = "1.17"
 }
 
+resource "aws_iam_role" "worker_role" {
+    name = "saga-ra3-worker-role"
+    
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
+resource "aws_iam_role_policy_attachment" "worker-node-eks-policy" {
+  role = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "worker-node-ecr-policy" {
+  role = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "worker-node-cni-policy" {
+  role = aws_iam_role.worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_node_group" "nodegroup" {
+    cluster_name = aws_eks_cluster.cluster.name
+    node_group_name = "nodegroup"
+    node_role_arn = aws_iam_role.worker_role.arn
+    subnet_ids = [
+        var.private_subnet_1,
+        var.private_subnet_2
+    ]
+    
+    scaling_config {
+        desired_size = 1
+        max_size     = 1
+        min_size     = 1
+    }
+
+    depends_on = [
+        aws_iam_role_policy_attachment.worker-node-eks-policy,
+        aws_iam_role_policy_attachment.worker-node-ecr-policy,
+        aws_iam_role_policy_attachment.worker-node-cni-policy
+  ]
+}
